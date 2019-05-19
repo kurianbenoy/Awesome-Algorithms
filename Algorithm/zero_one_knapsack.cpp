ee82eee766cb88a1570610380f052a8d4f311c08
Kurian Benoy <kurian.bkk@gmail.com>
2019-05-19 07:57:35 +0530

    Zero one knapsack problem using DP

--- Algorithm/zero_one_knapsack.cpp
+++ Algorithm/zero_one_knapsack.cpp
@@ -2,17 +2,27 @@
 using namespace std;
 int max(int a, int b) { return (a>b)? a:b; }
 
-int knapsack(int W, int wt[], int val[], int n)
-{
-	if(n==0 || W==0)
-		return 0;
-	
-	if(wt[n-1] >W)
-		return knapsack(W, wt, val, n-1);
-	else 
-		return max(val[n-1] + knapsack(W-wt[n-1], wt, val, n-1), 
-                    knapsack(W, wt, val, n-1));
-}
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+   int i, w; 
+   int K[n+1][W+1]; 
+  
+   // Build table K[][] in bottom up manner 
+   for (i = 0; i <= n; i++) 
+   { 
+       for (w = 0; w <= W; w++) 
+       { 
+           if (i==0 || w==0) 
+               K[i][w] = 0; 
+           else if (wt[i-1] <= w) 
+                 K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]); 
+           else
+                 K[i][w] = K[i-1][w]; 
+       } 
+   } 
+  
+   return K[n][W]; 
+} 
 
 int main()
 {
@@ -30,9 +40,9 @@ int main()
 		}
 		for(int i=0;i<n;i++)
 			cin>>wt[i];
-		printf("%d", knapsack(W, wt, v, n));
-		}
+		printf("%d", knapSack(W, wt, v, n));
 		cout<<"\n";
+		}
 
 return 0;
 } 
