8bfe46c5ab9a6579519b98ed8e0236a726464d7a
kurianbenoy <kurian.bkk@gmail.com>
2019-06-02 16:45:12 +0530

    Add solution of longest common subsequence problem

--- Sudo_Placement/SP_dynamicProgramming/longest_common_subsequence.cpp
+++ Sudo_Placement/SP_dynamicProgramming/longest_common_subsequence.cpp
@@ -3,25 +3,37 @@ using namespace std;
 
 int lcs(char *X, char *Y, int m, int n)
 {
-	if(m==0 || n==0)
-		return 0;
-	if(X[m-1] == Y[n-1])
-		return(1+ lcs(X, Y, m-1, n-1));
-	else
-		return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+	int L[m+1][n+1];
+	int i, j;
+
+	for(i=0;i<=m;i++)
+	{
+		for(j=0;j<=n;j++)
+		{
+			if(i ==0 || j==0)
+				L[i][j] = 0;
+			else if(X[i-1] == Y[j-1])
+				L[i][j] = L[i-1][j-1] + 1;
+			else
+				L[i][j] = max(L[i-1][j], L[i][j-1]);
+		}
+	}
+	return L[m][n];
 }
+
 
 int max(int a, int b)
 	{
-		return  ((a>b)? a: b);
+		return  (a>b)? a: b;
 	}
 int main()
 {
-	int T,n,m;
+	int T;
 	cin>>T;
-	char str1[200],str2[200];
-	for(int i=0;i<T;i++)
+	while(T--)
 	{
+		int m,n;
+		char str1[200],str2[200];
 		cin>>m>>n;
 		cin>>str1;
 		cin>>str2;
