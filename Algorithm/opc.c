45c8572aacc9de0cebc7562543200cbc86e54d81
kurianbenoy <kurian.bkk@gmail.com>
2019-08-21 22:10:56 +0530

    update ..

--- Algorithm/opc.c
+++ Algorithm/opc.c
@@ -1,10 +1,46 @@
 #include<stdio.h>
 
+void pascal f(int i,int j,int k)
+{
+  printf("%d %d %d",i, j, k);
+}
+
+void cdecl f(int i,int j,int k)
+{
+  printf("%d %d %d",i, j, k);
+}
+
 int main()
 {
-	int x;
+
+	int i=10; 
+    f(i++,i++,i++); 
+    printf(" %d\n",i); 
+    i=10; 
+    f(i++,i++,i++); 
+    printf(" %d",i); 
+
+/*	int a[10];
+	printf("%d",*a+1-*a+3);
+
+/*
+    unsigned int i=10; 
+    while(i-- >= 0) 
+        printf("%u ",i); 
+    return 0; 
+
+
+/*while(1){
+	if(printf("%d",printf("%d")))
+		break;
+	else
+		continue;
+	}
+	return 0;
+
+/*	int x;
 	printf("%d",scanf("%d",&x));
+	ans: 1*/
 	/* Suppose that input value given
 		for above scanf is 20 */
-	return 1;
 }
