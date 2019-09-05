74d75c647a7222b748220f90a0d5969a2b46a89f
kurianbenoy <kurian.bkk@gmail.com>
2019-09-05 07:52:57 +0530

    floyds triangle

---
+++ Sudo_Placement/patterns/floyds_triangle.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n,no=1;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<no;
+			++no;
+		}
+	cout<<endl;
+}
+
+}
